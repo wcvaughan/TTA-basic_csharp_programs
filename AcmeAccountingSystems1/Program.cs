2856e7a9846ee953c116620948d0ad83fce1bd57
wcvaughan <109022755+wcvaughan@users.noreply.github.com>
2022-09-23 15:45:31 -0800

    Add files via upload

---
+++ AcmeAccountingSystems1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AcmeAccountingSystems1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please knock on the door. (Type in \"Knock Knock\")");
+            Console.ReadLine();
+            Console.WriteLine("Welcome to Acme Accounting Systems! \n Remember, we're \"accounting\" on you!");
+            Console.Read();
+            Console.WriteLine("Are you still here?");
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+        Console.WriteLine("When people say, \"Hello!\", they might want to ask \"How are you?\"");
+        Console.Read();
+    }
+}
