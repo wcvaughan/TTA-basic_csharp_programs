88e41fc848e4fd69ea2c39d44c66bb268d4da372
wcvaughan <chrisvaughanlds@gmail.com>
2022-09-24 23:17:22 -0800

    More work done!

--- BooleanLogic/Program.cs
+++ BooleanLogic/Program.cs
@@ -14,12 +14,26 @@ namespace CarInsuranceApproval
             //What is your age ?
             Console.WriteLine("What is your age?");
             int userAge = Convert.ToInt32(Console.ReadLine());
+            if (userAge <= 15)
+            {
+                Console.WriteLine("Wait until you're old enough punk!");
+            }
+            else 
             //Have you ever had a DUI?
             Console.WriteLine("Have you ever had a DUI? Please answer with true or false only, otherwise this whole thing will break. Then you'll never get your car inn-sewer-ants");
             bool hadDUI = bool.Parse(Console.ReadLine());
+            if (hadDUI == true)
+            {
+                Console.WriteLine("Your premium is about to skyrocket!");
+            }
+            else
             //How many speeding tickets do you have?
             Console.WriteLine("How many speeding tickets have you had?");
             int userSpeedDemon = Convert.ToInt32(Console.ReadLine());
+            if (userSpeedDemon > 1)
+            {
+                Console.WriteLine("You gotta slow down buddy, but go on");
+            }
             //Applicants must be over 15 years old.
             bool isOldEnough = userAge > 15;
             //Applicants must not have any DUI’s.
@@ -28,8 +42,19 @@ namespace CarInsuranceApproval
             bool noSpeedDemon = userSpeedDemon < 3;
             //Is the applicant qualified?
             Console.WriteLine("Are you qualified?");
-            Console.WriteLine(isOldEnough && noDUI && noSpeedDemon);
+            if (isOldEnough && noDUI && noSpeedDemon)
+            {
+                Console.WriteLine("YOU WON!!!");
+            }
+            else if (noSpeedDemon != true || isOldEnough != true || noDUI != true)
+            {
+                Console.WriteLine("Something's gotta change");
+            }
+            int oldie = 65;
+            string insult = userAge > oldie ? "We need to get you a new driver's test" : "Get on the road!";
+            Console.WriteLine(insult);
             Console.ReadLine();
+
         }
     }
 }
---
+++ Branching/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The first line of the program must be: “Welcome to Package Express. Please follow the instructions below.”
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            //The user must then be prompted for the package weight.
+            Console.WriteLine("How much does your package weigh? Please use the conveniently located scale at your own home.");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+            //If the weight is greater than 50, display the error message, “Package too heavy to be shipped via Package Express.Have a good day.” At this point the program would end.
+            string weightResult = packageWeight > 50 ? "Package too heavy to be shipped via Package Express.Have a good day." : "Wonderful! Continue";
+            Console.WriteLine(weightResult);
+            //The user must then be prompted for the package width.
+            Console.WriteLine("What is the width of your package?");
+            int packageWidth = Convert.ToInt32(Console.ReadLine());
+            //Then the package height.
+            Console.WriteLine("Now your package height.");
+            int packageHeight = Convert.ToInt32(Console.ReadLine());
+            //Then the package length.
+            Console.WriteLine("Now your package length.");
+            int packageLength = Convert.ToInt32(Console.ReadLine());
+            //If the dimensions total greater than 50, display the error message, “Package too big to be shipped via Package Express.” At this point the program would end.
+            string dimensionsResult = (packageWidth + packageHeight + packageLength) > 50 ? "Package too big to be shipped via Package Express." : "We are THRILLED to ship your package";
+            //Next, multiply the three dimensions(height, width, &length) together, and multiply the product by the weight.Finally, divide the outcome by 100.
+            int finalOutcome = ((packageWidth * packageHeight * packageLength) * packageWeight) / 100;
+            //The result of that calculation is the quote.
+
+            //Display the quote to the user as a dollar amount.
+            Console.WriteLine("Your final quote is $" + finalOutcome);
+            Console.ReadLine();
+        }
+    }
+}
