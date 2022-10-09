6df2a709997e2bb32c396d17eb6bcdbbdcc0e21a
wcvaughan <chrisvaughanlds@gmail.com>
2022-10-09 01:20:38 -0800

    Kind of

---
+++ MethodAssignment/DosNumeros.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class DosNumeros
+    {
+        //Create a class. In that class, create a method that takes two integers as parameters.
+        //Make one of them optional. Have the method do a math operation and return an integer result.
+
+        public int Algebraic(int x, int y)
+        {
+            return x + 42 - y;
+        }
+    }
+}
---
+++ MethodAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main() method of the console app, instantiate the class.
+
+            var DosNumeros = new DosNumeros();
+
+            //Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second number.
+
+            Console.WriteLine("Give me two numerals that you thought of yesterday. But only one at a time. \nYou also could just give me one.");
+
+            int userSub = Convert.ToInt32(Console.ReadLine());
+            int userSub2 = Convert.ToInt32(Console.ReadLine());
+
+            //Call the method in the class, passing in the one or two numbers entered.
+
+            int result = DosNumeros.Algebraic(userSub, userSub2);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+
+            //Try various combinations of numbers on the code, including having no second number.
+
+            //Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
+        }
+    }
+}
