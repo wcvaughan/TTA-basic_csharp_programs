d8c2cb1aaa023f9da6232bc0bb996fafb7818489
wcvaughan <chrisvaughanlds@gmail.com>
2022-10-11 09:54:05 -0800

    what?

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -10,6 +10,8 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            TwentyOneGame game = new TwentyOneGame();
+            
             Deck deck = new Deck();
             deck.Shuffle();
 
