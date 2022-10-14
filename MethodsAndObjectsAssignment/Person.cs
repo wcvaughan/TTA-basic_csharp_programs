56dcc8c37939cee303996e017da110033dc38f71
wcvaughan <chrisvaughanlds@gmail.com>
2022-10-14 13:01:27 -0800

    More work

---
+++ MethodsAndObjectsAssignment/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    public class Person
+    {
+        //Create a class called Person and give it two properties, each of data type string. One called FirstName, the other LastName.
+        //Give this class a void method called SayName() that takes no parameters and simply writes the person's
+        //full name to the console in the format of: “Name: [full name]”.
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ MethodsAndObjectsAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inside of the Main method, instantiate and initialize an Employee object with a first name of “Sample” and a last name of “Student”.
+
+            Employee employee = new Employee();
+
+            employee.FirstName = "Sample";
+
+            employee.LastName = "Student";
+
+            //Call the superclass method SayName() on the Employee object.
+
+            employee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
