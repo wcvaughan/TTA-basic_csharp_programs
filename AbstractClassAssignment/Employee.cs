e913b93ceb054a5738bcf9791e9e3b94b5412add
wcvaughan <chrisvaughanlds@gmail.com>
2022-10-17 01:51:46 -0800

    Interface, polymorphism, etc

--- AbstractClassAssignment/Employee.cs
+++ AbstractClassAssignment/Employee.cs
@@ -8,7 +8,7 @@ namespace AbstractClassAssignment
 {
     //Create another class called Employee and have it inherit from the Person class.
 
-    public class Employee : Person
+    public class Employee : Person, IQuittable
     {
         //Implement the SayName() method inside of the Employee class.
         public override void SayName()
@@ -16,5 +16,11 @@ namespace AbstractClassAssignment
             base.SayName();
         }
         //Inside the Main() method, instantiate an Employee object with firstName “Sample” and lastName “Student”. Call the SayName() method on the object.
+
+        //Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
+        public void Quit(Employee employee)
+        {
+            Console.WriteLine(employee.firstName + " has quit");
+        }
     }
 }
--- AbstractClassAssignment/Program.cs
+++ AbstractClassAssignment/Program.cs
@@ -29,8 +29,18 @@ namespace AbstractClassAssignment
             employee.SayName();
 
             Console.ReadLine();
-            //Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
 
+            //Create an interface called IQuittable and have it define a void method called Quit().
+
+            //Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
+
+            //Use polymorphism to create an object of type IQuittable and call the Quit() method on it.
+            //Hint: an object can be of an interface type if it implements that specific interface.
+            IQuittable quittable = new Employee();
+            quittable.Quit(employee);
+            Console.ReadLine();
+
+            //Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
         }
     }
 }
