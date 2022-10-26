a1cbd1737ab5ac335e3b905d72721bdfa103b238
wcvaughan <chrisvaughanlds@gmail.com>
2022-10-26 12:35:12 -0800

    struct assignment

---
+++ StructAssignment1/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment1
+{
+    public struct Number
+    {
+        //Create a struct called Number and give it the property “Amount” and have it be of data type decimal.
+        public decimal Amount { get; set; }
+
+    }
+}
---
+++ StructAssignment1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main() method, create an object of data type Number and assign an amount to it.
+
+            Number number1 = new Number();
+
+            number1.Amount = 5.5m;
+
+            //Print this amount to the console.
+
+            Console.WriteLine(number1.Amount);
+            Console.ReadLine();
+
+            //Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
+    }
+}
+}
