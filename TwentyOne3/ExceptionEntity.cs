c8ec46834ea5532a7762d5d138dd4532106bb27b
wcvaughan <chrisvaughanlds@gmail.com>
2022-12-18 23:05:41 -0900

    Final TwentyOne Game

---
+++ TwentyOne3/ExceptionEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne3
+{
+    class ExceptionEntity
+    {
+        public int Id  { get; set; }
+        public string ExceptionType { get; set; }
+        public string ExceptionMessage { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
